399b7de94679f6bad3b47ab82665e529799e7c4b
Cahuana Vera Diego Joaquin <dcahuanav@unsa.edu.pe>
2025-09-17 17:50:15 -1100

    Primer commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let mensaje = "¡Hola, mundo!";
+
+function saludar() {
+  console.log(mensaje);
+}
+
+function mostrarNumero(numero) {
+  if (numero > 10) {
+    console.log("El número es mayor que 10.");
+  } else {
+    console.log("El número no es mayor que 10.");
+  }
+}
+
+saludar(); 
+
+
+mostrarNumero(5); 
+
+mostrarNumero(20); 
